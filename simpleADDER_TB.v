01114d26844c9d75a6f54d12484959254c8680cd
Matteo Brini <brinimatteo@gmail.com>
2023-10-02 15:24:23 +0200

    Improved testbench and simpler logic for module simpleADDER

--- simpleADDER_TB.v
+++ simpleADDER_TB.v
@@ -2,14 +2,17 @@
 
 module simpleADDER_TB;
 
+    // LOCAL VARIABLES
     reg signed [24:0] in_0;
     reg signed [24:0] in_1;
     reg signed [24:0] in_2;
     reg signed [24:0] in_3;
     wire signed [24:0] out;
     reg clk;
+    reg signed [24:0] check_sum;
 
-    simpleADDER simpleADDERMODULE (
+    // DUT CONNECTION
+    simpleADDER simpleADDER_MODULE (
         .CLK    (clk),
         .WORD_0 (in_0),
         .WORD_1 (in_1),
@@ -17,26 +20,42 @@ module simpleADDER_TB;
         .WORD_3 (in_3),
         .RES    (out));
 
+    // CLOCK LOGIC
     always #5 begin
+        check_sum <= in_0 + in_1 + in_2 + in_3;
         clk <= ~clk;
     end
 
+    // TESTBENCH
     initial begin
+        $display("time\t clk\t\t CHECK\t\t O");
+        $monitor("%g\t %b\t %d\t %d", $time, clk, check_sum, out);
         clk = 0;
         in_0 = 0;
         in_1 = 0;
         in_2 = 0;
         in_3 = 0;
 
-        #3  in_0 <= 5;
-        #1  in_1 <= 4;
-        #1  in_2 <= 4;
-        #1  in_3 <= 4;
-        #20 $display("RES: %d", out);
-        #10;
+        // TEST 1 -------------------------------
+        #10 $display("TEST 1: START");
+            in_0 = 1;
+            in_1 = 2;
+            in_2 = 3;
+            in_3 = 4;
+        #10 $display("TEST 1: STOP");
+        // --------------------------------------
+        
+        // TEST 2 -------------------------------
+        #20 $display("TEST 2: START");
+            in_0 = -1;
+            in_1 = 0;
+        #1  in_2 = 5;
+        #1  in_0 = 0;
+        #10 $display("TEST 2: STOP");
+        // --------------------------------------
+        
+        #20;
         $finish;
     end
-
-
 
 endmodule
