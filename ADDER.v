53aaf613046d9a7e8ff02870abc628418a6885c6
Matteo Brini <brinimatteo@gmail.com>
2023-10-02 15:23:54 +0200

    Implemented vector of inputs and for cicles

--- ADDER.v
+++ ADDER.v
@@ -1,23 +1,28 @@
-module ADDER ( input    [24:0] WORD_0,
-                        [24:0] WORD_1,
-                        [24:0] WORD_2,
-                        [24:0] WORD_3,
-                        [24:0] WORD_4,
-                        [24:0] WORD_5,
-                        [24:0] WORD_6,
-                        [24:0] WORD_7,
-                        [24:0] WORD_8,
-                        [24:0] WORD_9,
-                        [24:0] WORD_10,
-                        [24:0] WORD_11,
-                        [24:0] WORD_12,
-                        [24:0] WORD_13,
-                        [24:0] WORD_14,
-                        [24:0] WORD_15,
-                        CLK,
-                output  [24:0] RES);
+module ADDER ( input    signed [24:0] WORD[15:0],
+               input    CLK,
+               output   wire signed [24:0] RES);
+
+                reg signed [24:0] SUM[14:0];
+
+                integer i;
+                initial begin
+                    for (i = 0; i < 16; i = i + 1) begin
+                        SUM[i] = 0;
+                    end
+                end
+
+                assign RES = SUM[14];
 
                 always @ (posedge CLK) begin
-                               
+                    for (i = 0; i < 8; i = i + 1) begin
+                        SUM[i] <= WORD[i] + WORD[i + 8];
+                    end
+                    for (i = 0; i < 4; i = i + 1) begin
+                        SUM[i + 8] <= SUM[i] + SUM[i + 4];
+                    end
+                    for (i = 0; i < 2; i = i + 1) begin
+                        SUM[i + 12] <= SUM[i + 8] + SUM[i + 10];
+                    end
+                    SUM[14] <= SUM[12] + SUM[13];
                 end
 endmodule
